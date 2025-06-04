7a11fd434afdfa7f68aafff988926a57d19b499a
Paulo Grabin <plgrabin@gmail.com>
2025-06-04 14:57:58 -0300

    remove temp

--- temp/hybris/cockpitng/cockpitng/cng/scripts/backspace-intercept.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-    $(document).keydown(function(e){
-        var valid = $(document.activeElement).is("input:focus, textarea:focus");
-        if(e.keyCode === 8 && !valid)
-        { return false; }
-    });
-});
--- temp/hybris/cockpitng/cockpitng/cng/scripts/vendor/survey-button/cx-qtx-survey-button.mjs
+++
@@ -1,5 +0,0 @@
-import { W as r } from "./CxQtxSurveyButton.96c49aa8.mjs";
-export {
-  r as CxQtxSurveyButton
-};
-//# sourceMappingURL=cx-qtx-survey-button.mjs.map
